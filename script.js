1e939d843fae587ed73a0f04f039fafdaafcc4d9
PassosEduardo <dudusegui@gmail.com>
2022-11-17 10:59:56 -0300

    novo commit

--- script.js
+++ script.js
@@ -5,19 +5,43 @@ valorTotal.innerHTML = total
 
 const h2 = document.getElementById('h2')
 const form = document.getElementById("formulario");
+const btnAdd = document.getElementById('btnAdd')
 const dado = document.forms.formulario.afazer
 
-form.addEventListener('submit', ($event) => 
-{
+btnAdd.addEventListener('click', AdicionaEvento);
+
+
+function AdicionaEvento ($event) {
     total++;
     $event.preventDefault();
     console.log(dado.value)
 
     const item = document.createElement('ol');
     item.innerText = dado.value
-    h2.after(item)
+    lista.appendChild(item);
 
     valorTotal.innerHTML = total
-
 }
-)
+
+
+
+const removeButton = document.getElementById('btnDelete')
+
+removeButton.addEventListener('click',removeTudo);
+
+const lista = document.getElementById('lista')
+const itemsToBeRemoved = document.getElementsByTagName('ol');
+
+function removeTudo($event)
+{
+    $event.preventDefault();
+    
+    lista.innerHTML = null;
+
+    total = 0 ;
+    valorTotal.innerHTML = total
+
+    
+    
+}
+
